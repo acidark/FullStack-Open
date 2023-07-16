39b6785dbed0464c15013afeaee303bc1ebc1348
eoeo <eoeo@localhost.localdomain>
2023-07-16 19:20:17 +0300

    1.3

--- part1/courseinfofix/src/App.js
+++ part1/courseinfofix/src/App.js
@@ -1,36 +1,29 @@
 const Part = (props) =>{
   return(
-    <div>{props.name} {props.exer}</div>
+    <div>{props.part.name} {props.part.exec}</div>
   )
 }
-
 const Content = (props) =>{
+  console.log(props)
   return(
       <div>
-      <Part name={props.part1} exer={props.exec1} />
-      <Part name={props.part2} exer={props.exec2} />
-      <Part name={props.part3} exer={props.exec3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
       </div>
   )
 }
-
 const App = () => {
   const course = 'Half stack application development'
-  const part1 = 'Fundamentals of react'
-  const exec1 = 10
-  const part2 = 'Using props to pass data'
-  const exec2 = 11
-  const part3 = 'State of component'
-  const exec3 = 20
+  const part1 = {name:'Fundamentals of react',exec:10}
+  const part2 = {name:'Using props to pass data',exec:11}
+  const part3 = {name:'State of component',exec : 20}
   return (
     <div>
       <h1>{course}</h1>
-
-      <Content part1={part1} exec1={exec1} part2={part2} exec2={exec2} part3={part3} exec3={exec3} />
-
-      <p>Number of exercises {exec1+exec2+exec3}</p>
+      <Content part1={part1} part2={part2} part3={part3} />
+      <p>Number of exercises {part1.exec+part2.exec+part3.exec}</p>
     </div>
   );
 }
-
 export default App;
