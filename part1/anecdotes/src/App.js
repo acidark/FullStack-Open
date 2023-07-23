f57d4567e64cb2fe7f7f82d0d0fc7097678785c8
eoeo <eoeo@localhost.localdomain>
2023-07-23 14:53:59 +0300

    1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
+  // console.log({maximum})
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -14,6 +15,7 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [anecdoteScore,setAnecdoteScore] = useState(new Uint8Array(anecdotes.length))
+  const [maxim,setMaxi] = useState(0)
   const nextSelect = () => {
     let rand = getRandom()
     while(selected === rand){ (
@@ -22,26 +24,48 @@ const App = () => {
     setSelected(rand)
 
   }
+   const maximum = () => {
+    let max = anecdoteScore[0];
+    let maxi = 0;
+    anecdoteScore.forEach(function(val,index){
+      if(val > max){
+        max = val
+        maxi = index
+       }
+      })
+     return {
+      max : max,
+      maxi : maxi
+     }
+     //setMaxi(maxi)
+   }
 
-  const vote = () =>{
+  const result = maximum()
+  const vote = () =>{    
     const copy = [...anecdoteScore]
     copy[selected]+=1
     setAnecdoteScore(copy)
-    console.log(anecdoteScore) 
   }
-
   const getRandom = () =>{
      let min = Math.ceil(0)
      let max = Math.floor(anecdotes.length-1)
      let random = Math.floor(Math.random()*(max-min+1)+min)
     return random;
    }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <p>has {anecdoteScore[selected]} votes</p>
       <div>
-      <button onClick={nextSelect}>next</button>
-      <button onClick={vote}>vote</button>
+        <p>
+          <button onClick={nextSelect}>next</button>
+          <button onClick={vote}>vote</button>     
+        </p> 
+      <h1>Most voted anecdote</h1>       
+        {anecdotes[result.maxi]}
+        <p>has {result.max} votes</p>
       </div>
     </div>
   )
