4c1d95e0b1f3a95628c68a981dfecdfef0b7ac42
eoeo <eoeo@localhost.localdomain>
2023-07-23 12:28:25 +0300

    anecdotes 1.13

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const nextSelect = () => {
+    let rand = getRandom()
+    while(selected === rand){ (
+        rand = getRandom()
+    )}
+    setSelected(rand)
+    // anecdoteScore[rand]+=1
+    const copy = [...anecdoteScore]
+    copy[rand]+=1
+
+    setAnecdoteScore(copy)
+    console.log(anecdoteScore)
+ 
+  }
+  const [selected, setSelected] = useState(0)
+  const [anecdoteScore,setAnecdoteScore] = useState(new Uint8Array(anecdotes.length))
+  // const [anecdoteScore,setAnecdoteScore] = useState([])
+  // const copy =[...anecdoteScore]
+  const getRandom = () =>{
+     let min = Math.ceil(0)
+     let max = Math.floor(anecdotes.length-1)
+     let random = Math.floor(Math.random()*(max-min+1)+min)
+    return random;
+   }
+
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <div>
+      <button onClick={nextSelect}>next</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
-
   const StatisticsLine = ({value,text}) => {
-
     return(
       <tr>
         <td>{value}</td>
@@ -10,9 +8,6 @@ import { useState } from "react";
     )
   }
   const Statistics = ({good,bad,neutral}) => {
-    // console.log(props)
-    // console.log(good,bad,neutral)
-    // const total = () = {}
     const total = () => good+bad+neutral
     if (total()===0){
       return (
@@ -21,9 +16,6 @@ import { useState } from "react";
     }
     return(
       <div>
-        {/* <p>{good} good</p>
-        <p>{neutral} neutral</p>
-        <p>{bad} bad</p> */}
       <table>
         <tbody>
         <StatisticsLine value={good} text="good" />
@@ -32,20 +24,6 @@ import { useState } from "react";
         <StatisticsLine value={total()} text="total" />
         <StatisticsLine value={good/total()} text="% positive" />
         <StatisticsLine value={(good-bad)/total()} text="average" />
-        {/* <table> */}
-          {/* <tbody> */}
-        
-            {/* <tr> */}
-              {/* {total()} all */}
-            {/* </tr> */}
-            {/* <tr> */}
-             {/* {good/total()}% positive */}
-            {/* </tr> */}
-            {/* <tr> */}
-              {/* {(good-bad)/total()} avg */}
-            {/* </tr> */}
-          {/* </tbody> */}
-        {/* // </table> */}
           </tbody>
         </table>
       </div>
@@ -53,77 +31,39 @@ import { useState } from "react";
   }
   const Button =({handle,text}) => <button onClick={handle}>{text}</button>
   const App =() =>{
-  // const [bad,setBad] = useState(0)
-  // const [neutral,setNeutral] = useState(0)
-  // const [good,setGood] = useState(0)
-
   const [click,setClicks] = useState({
     good:0,bad:0,neutral:0
   })
-
   const handleSetBad = ()=>{
-    // console.log(click.bad)
-//    const newClick = {
+
       setClicks({
       ...click,
       bad:click.bad+1
-      //neutral:click.neutral,
-      //good:click.good
-      //good:click.good,
-      //neutral:click.neutral
+
     })
-    //setClicks(newClick)
   }
   const handleSetGood = () =>{
-    //const newClick = {
     setClicks(
     {...click,
       good : click.good+1,
-//      bad:click.bad,
-//      neutral:click.neutral
-
     })
-    //setClicks(newClick)
   }
 
   const handleSetNeutral = () =>{
     setClicks({
-    //const newClick = {
       ...click,
         neutral:click.neutral+1,
-//      bad:click.bad,
-//      good:click.good
-    })
-    //setClicks(newClick)
-  }
-  //const sumOfClicks = (click.bad+click.good+click.neutral)
 
+    })
+  }
   return (
     <div>
       <h1>Statistics</h1>
-      {/* if(click.total===0) return (<div>No feedback given</div>)  */}
-      
-      {/* <p>{click.good} good</p>
-      <p>{click.neutral} neutral</p>
-      <p>{click.bad} bad</p>
-      <p>{click.total} all</p> */}
-      {/* <StatisticsLine good={click.good} bad={click.bad} neutral={click.neutral} /> */}
       <Statistics good={click.good} bad={click.bad} neutral={click.neutral} /> 
-
-      {/* <p>{click.good/click.good+click.bad+click.neutral} %positive</p> */}
-      {/* <p>{(click.good+click.bad)/sumOfClicks} avg</p> */}
-
       <h1>give feedback</h1>
       <Button handle={handleSetGood} text="good" />
       <Button handle={handleSetNeutral} text="neutral" />
-      <Button handle={handleSetBad} text="bad" />
-      
-      
-      {/* <button onClick={()=>setGood(good+1)}>good</button> */}
-      {/* <button onClick={handleSetBad}>bad</button> */}
-      {/* <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button> */}
-      
+      <Button handle={handleSetBad} text="bad" />     
     </div>
  );
 }
