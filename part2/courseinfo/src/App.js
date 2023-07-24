8feb33d5208396d97bf7eae56e4ee19c234248e6
eoeo <eoeo@localhost.localdomain>
2023-07-24 17:17:53 +0300

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,51 +1,45 @@
+import Course from './components/Course'
+// const Part = ({part}) =>{ 
+//   return( 
+//   <p>
+//   {part.name} {part.exercises}
+//   </p> 
+//   )
+// }
 
-const Part = ({part}) =>{
-  // const numbers = [1,2,3,4]
-  // const sum = numbers.reduce((s,p)=>{
-  // console.log(p)
-  // return s+p},0);
-  
-  return( 
-  <p>
-  {part.name} {part.exercises}
-  </p>
-  
-  )
-  }
+// const Header = ({header}) => {
+//   console.log(header)
+//   return(
+//   <div>
+//     <h2>{header}</h2>
+//   </div>
+//   )
+// }
 
-const Header = ({header}) => {
-  console.log(header)
-  return(
-  <div>
-    <h2>{header}</h2>
-  </div>
-  )
-}
+// const Content = ({parts}) => {
+//   return( 
+//     <div>
+//       {parts.map((part) => <Part key={part.id} part={part} />)}      
+//     </div>
+//   )
+// }  
+// const Course = ({course}) => {
+//   // console.log(course.name)
+//     // console.log(courses[0].parts[0].exercises)
+//     const total = course.parts
+//     .map(part=>part.exercises)
+//     .reduce((s,p) => s+p)
+//     return(
+//     <div>
+//       {/* {course.map((course)=> <Header key={course.id} course={course} />)} */}
 
-const Content = ({parts}) => {
-  return( 
-    <div>
-      {parts.map((part) => <Part key={part.id} part={part} />)}      
-    </div>
-   )
- }  
-const Course = ({course}) => {
-  // console.log(course.name)
-    // console.log(courses[0].parts[0].exercises)
-    const total = course.parts
-    .map(part=>part.exercises)
-    .reduce((s,p) => s+p)
-    return(
-    <div>
-      {/* {course.map((course)=> <Header key={course.id} course={course} />)} */}
-
-      {/* {courses.map((par))} */}
-      <Header header={course.name} />
-      <Content parts={course.parts} />
-      <p><b>Number of exercises is {total}</b></p>
-    </div>
-    )
-}
+//       {/* {courses.map((par))} */}
+//       <Header header={course.name} />
+//       <Content parts={course.parts} />
+//       <p><b>Number of exercises is {total}</b></p>
+//     </div>
+//     )
+// }
 const App = () => {
   const courses = [
     {
@@ -98,7 +92,4 @@ const App = () => {
     </div>
   )
 }
-
-
-
 export default App
---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,43 @@
+const Part = ({part}) =>{ 
+  return( 
+  <p>
+  {part.name} {part.exercises}
+  </p> 
+  )
+}
+
+const Header = ({header}) => {
+  console.log(header)
+  return(
+  <div>
+    <h2>{header}</h2>
+  </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return( 
+    <div>
+      {parts.map((part) => <Part key={part.id} part={part} />)}      
+    </div>
+  )
+}  
+const Course = ({course}) => {
+    // console.log(course.name)
+      // console.log(courses[0].parts[0].exercises)
+      const total = course.parts
+      .map(part=>part.exercises)
+      .reduce((s,p) => s+p)
+      return(
+      <div>
+        {/* {course.map((course)=> <Header key={course.id} course={course} />)} */}
+  
+        {/* {courses.map((par))} */}
+        <Header header={course.name} />
+        <Content parts={course.parts} />
+        <p><b>Number of exercises is {total}</b></p>
+      </div>
+      )
+  }
+
+  export default Course
