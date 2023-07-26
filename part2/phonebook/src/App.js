1b5c83ab133beb065e819ca3825ad9a40e1b22ab
eoeo <eoeo@localhost.localdomain>
2023-07-26 19:32:25 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,19 +2,33 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number:NaN  
-  }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newSearch,setNewSearch] = useState('') 
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
+  // const [showAll,setShowAll] = useState(true)
+
+  const personToShow = persons.filter((person)=>person.name.toLowerCase().includes(newSearch.toLowerCase()))
+  // console.log(personToShow2)
+  // const found = () =>{
+  //   const ff = persons.some(person=>person.name === newSearch)
+  //   ff ? setShowAll(false) : setShowAll(true)
+  //   console.log(ff)
+  // }
   const handleNewName = (event) => {
-    // console.log(event.target.value)
     setNewName(event.target.value)
   }
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
+
+   const handleNewSearch = (event) => {
+     setNewSearch(event.target.value)
+   }
   const exists = (props) => {
     return persons.find(person => person.name == props)
   }
@@ -26,36 +40,36 @@ const App = () => {
     setPersons(persons.concat(newObject))
     setNewName('')
     setNewNumber('')
-  };     
+  };
+  // const findPerson = ''     
   const checkingParams = (event) =>{
     event.preventDefault()
     const foundOrNot = exists(newName)
     const toAdd = foundOrNot ? window.alert(`${newName} already in phonebook`) : addPerson();
     
-    //   const newObject = {
-    //   name : newName
-    // }
-    // setPersons(persons.concat(newObject))
-    // setNewName('')
   }
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter shown by
+        <div>
+          <input value={newSearch} onChange={handleNewSearch} />
+        </div>
+      <h2>Add new</h2>
       <form onSubmit={checkingParams}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-          
+        </div>          
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person=><li key={person.name}><div>{person.name} {person.number}</div></li>)}
+        {personToShow.map(person=><li key={person.name}><div>{person.name} {person.number}</div></li>)}
       </ul>
     </div>
   )
--- part2/phonebook/src/notes.md
+++ part2/phonebook/src/notes.md
@@ -1,2 +1,5 @@
 1. in js template literals starts and ends with ``, not "" , ''
   !!!!!!!
+
+2. comparing or executing conditional operators on object properties are done without curly braces:
+  const personToShow = showAll ? persons : persons.filter((person)=>person.name === newSearch)
