1015c2fdf805c57010c5df0c802d9a8b6a98c92f
eoeo <eoeo@localhost.localdomain>
2023-09-08 22:43:24 +0300

    little fix

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,17 +5,19 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
-
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
 .then(()=>{
     logger.info('connected to db')
+}).catch(error=>{
+    logger.error('error connecting to MongoDB',error.message)
 })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 app.use('/api/blog',blogsRouter)
-
-
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = app
