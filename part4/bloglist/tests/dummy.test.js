b139365d930a626c732637bb3ff34bf7bd7aa7f3
eoeo <eoeo@localhost.localdomain>
2023-09-20 20:35:26 +0300

    first backend test, checking if blog returns json objects

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -1,11 +1,3 @@
-// const listHelper = require('../utils/list_helper')
-
-// test('dummy returns one',() => {
-//   const blogs = []
-//   const result = listHelper.dummy(blogs)
-//   expect(result).toBe(1)
-// })
-
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
