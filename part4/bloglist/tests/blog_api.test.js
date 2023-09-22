6148a9f34d18c0c271fc09eefceb6c3af84c254b
eoeo <eoeo@localhost.localdomain>
2023-09-22 15:28:17 +0300

    changing parallel asynchronous fnction of (promise.all)to a ordered one using a for loop

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,45 +3,55 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('../tests/blog_api_helper')
 
-
-test('blogs are returned as json', async () => {
-  await api
-   .get('/api/blog')
-   .expect(200)
-   .expect('Content-Type',/application\/json/)
-
-})
-
-test('blogs return id property',async () => {
-  const response = await api.get('/api/blog')
-  console.log(response)
-  expect(response.body[0].id).toBeDefined()
-})
-
-test('blog can be added to db',async () => {
-  const numberOfBlogsAtStart = await Blog.find({})
-  // console.log(numberOfBlogs)
-  const newBlog = {
-    title : 'new blog',
-    author : 'new author',
-    url : 'test.com',
-    likes : 55555
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
   }
-  await api
-  .post('/api/blog')
-  .send(newBlog)
-  .expect(201) 
-  .expect('Content-Type',/application\/json/)
-  const numberOfBlogsAtEnd = await Blog.find({})
-  expect(numberOfBlogsAtEnd.length).toBe(numberOfBlogsAtStart.length+1)
-
-  const contents = numberOfBlogsAtEnd.map(blog => blog.title)
-  expect(contents).toContain(
-    "new blog"
-  )
 })
 
+describe('indexing data', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blog')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+  })
+
+  test('blogs return id property', async () => {
+    const response = await api.get('/api/blog')
+    console.log(response)
+    expect(response.body[0].id).toBeDefined()
+  })
+})
+
+describe('creating new blog', () => {
+  test('blog can be added to db', async () => {
+    const numberOfBlogsAtStart = helper.initialBlogs
+    // console.log(numberOfBlogs)
+    const newBlog = {
+      title: 'new blog',
+      author: 'new author',
+      url: 'test.com',
+      likes: 55555
+    }
+    await api
+      .post('/api/blog')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const numberOfBlogsAtEnd = await helper.blogsInDb()
+    expect(numberOfBlogsAtEnd.length).toBe(numberOfBlogsAtStart.length + 1)
+    const contents = numberOfBlogsAtEnd.map(blog => blog.title)
+    expect(contents).toContain(
+      "new blog"
+    )
+  })
+})
 afterAll(async () => {
   await mongoose.connection.close()
 })
